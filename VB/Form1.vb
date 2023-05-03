a3b10514cc313139703f5f243182e87f198c958e
DevExpressExampleBot <fakeEmail>
2023-05-03 19:28:13 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,38 +1,35 @@
-﻿Imports System
+Imports System
 Imports System.Drawing.Design
 Imports System.Windows.Forms
 Imports DevExpress.XtraReports.UserDesigner
-' ...
 
+' ...
 Namespace WindowsFormsApplication1
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             ' Create an End-User Designer form.
-            Dim designForm As New XRDesignForm()
-
+            Dim designForm As XRDesignForm = New XRDesignForm()
             ' Handle the DesignPanelLoaded event.
             AddHandler designForm.DesignMdiController.DesignPanelLoaded, AddressOf DesignMdiController_DesignPanelLoaded
             ' Load a report into the Designer.
             designForm.OpenReport(New XtraReport1())
-
             ' Show the End-User Designer form, modally.
             designForm.ShowDialog()
-
         End Sub
 
         Private Sub DesignMdiController_DesignPanelLoaded(ByVal sender As Object, ByVal e As DesignerLoadedEventArgs)
             ' Get the Toolbox service from the Designer host.
-            Dim ts As IToolboxService = DirectCast(e.DesignerHost.GetService(GetType(IToolboxService)), IToolboxService)
-
+            Dim ts As IToolboxService = CType(e.DesignerHost.GetService(GetType(IToolboxService)), IToolboxService)
             ' Add a new category to the Toolbox,
             ' and place a custom control into it.
-            Dim item As New ToolboxItem(GetType(ProgressBar))
+            Dim item As ToolboxItem = New ToolboxItem(GetType(ProgressBar))
             ts.AddToolboxItem(item, "New Category")
         End Sub
     End Class
--- VB/ProgressBar.vb
+++ VB/ProgressBar.vb
@@ -1,4 +1,4 @@
-﻿#Region "#Reference"
+'#Region "#Reference"
 Imports System.ComponentModel
 Imports System.Drawing
 Imports DevExpress.Utils.Design
@@ -6,14 +6,15 @@ Imports DevExpress.Utils.Serializing
 Imports DevExpress.XtraPrinting
 Imports DevExpress.XtraReports
 Imports DevExpress.XtraReports.UI
-' ...
-#End Region ' #Reference
 
-#Region "#Code"
+' ...
+'#End Region  ' #Reference
+'#Region "#Code"
 Namespace WindowsFormsApplication1
-' The DefaultBindableProperty attribute is intended to make the Position 
-' property bindable when an item is dropped from the Field List.
-    <ToolboxItem(True), DefaultBindableProperty("Position"), ToolboxBitmap24("WindowsFormsApplication1.ProgressBar24x24.png, WindowsFormsApplication11")> _
+
+    ' The DefaultBindableProperty attribute is intended to make the Position 
+    ' property bindable when an item is dropped from the Field List.
+    <ToolboxItem(True), DefaultBindableProperty("Position"), ToolboxBitmap24("WindowsFormsApplication1.ProgressBar24x24.png, WindowsFormsApplication11")>
     Public Class ProgressBar
         Inherits XRControl
 
@@ -24,43 +25,43 @@ Namespace WindowsFormsApplication1
         Private maxVal As Single = 100
 
         Shared Sub New()
-            DevExpress.XtraReports.Expressions.ExpressionBindingDescriptor.SetPropertyDescription(GetType(ProgressBar), "Position", New DevExpress.XtraReports.Expressions.ExpressionBindingDescription(New String() { "BeforePrint" }, 1000, New String(){}))
+            Expressions.ExpressionBindingDescriptor.SetPropertyDescription(GetType(ProgressBar), "Position", New Expressions.ExpressionBindingDescription(New String() {"BeforePrint"}, 1000, New String(-1) {}))
         End Sub
 
         Public Sub New()
-            Me.ForeColor = SystemColors.Highlight
+            ForeColor = SystemColors.Highlight
         End Sub
 
         ' Define the MaxValue property.
-        <DefaultValue(100), XtraSerializableProperty> _
-        Public Property MaxValue() As Single
+        <DefaultValue(100)>
+        <XtraSerializableProperty>
+        Public Property MaxValue As Single
             Get
-                Return Me.maxVal
+                Return maxVal
             End Get
+
             Set(ByVal value As Single)
-                If value <= 0 Then
-                    Return
-                End If
-                Me.maxVal = value
+                If value <= 0 Then Return
+                maxVal = value
             End Set
         End Property
 
         ' Define the Position property. 
-        <DefaultValue(0), Bindable(True), XtraSerializableProperty> _
-        Public Property Position() As Single
+        <DefaultValue(0), Bindable(True)>
+        <XtraSerializableProperty>
+        Public Property Position As Single
             Get
-                Return Me.pos
+                Return pos
             End Get
+
             Set(ByVal value As Single)
-                If value < 0 OrElse value > maxVal Then
-                    Return
-                End If
-                Me.pos = value
+                If value < 0 OrElse value > maxVal Then Return
+                pos = value
             End Set
         End Property
 
         ' Override the XRControl.CreateBrick method.
-        Protected Overrides Function CreateBrick(ByVal childrenBricks() As VisualBrick) As VisualBrick
+        Protected Overrides Function CreateBrick(ByVal childrenBricks As VisualBrick()) As VisualBrick
             ' Use this code to make the progress bar control 
             ' always represented as a Panel brick.
             Return New PanelBrick(Me)
@@ -70,25 +71,19 @@ Namespace WindowsFormsApplication1
         Protected Overrides Sub PutStateToBrick(ByVal brick As VisualBrick, ByVal ps As PrintingSystemBase)
             ' Call the PutStateToBrick method of the base class.
             MyBase.PutStateToBrick(brick, ps)
-
             ' Get the Panel brick which represents the current progress bar control.
             Dim panel As PanelBrick = CType(brick, PanelBrick)
-
             ' Create a new VisualBrick to be inserted into the panel brick.
-            Dim progressBar As New VisualBrick(Me)
-
+            Dim progressBar As VisualBrick = New VisualBrick(Me)
             ' Hide borders.
             progressBar.Sides = BorderSide.None
-
             ' Set the foreground color to fill the completed area of a progress bar.
             progressBar.BackColor = panel.Style.ForeColor
-
             ' Calculate the rectangle to be filled by the foreground color.
             progressBar.Rect = New RectangleF(0, 0, panel.Rect.Width * (Position / MaxValue), panel.Rect.Height)
-
             ' Add the VisualBrick to the panel.
             panel.Bricks.Add(progressBar)
         End Sub
     End Class
 End Namespace
-#End Region ' #Code
+'#End Region  ' #Code
