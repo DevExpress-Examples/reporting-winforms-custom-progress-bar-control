2e80080fb5a6c17b0978e9560e0f184b564e9a8e
DevExpressExampleBot <fakeEmail>
2023-07-19 20:00:09 +0400

    Source auto update [skip ci]

--- VB/ProgressBar.vb
+++ VB/ProgressBar.vb
@@ -1,20 +1,21 @@
-﻿#Region "#Reference"
+'#Region "#Reference"
 Imports System.ComponentModel
 Imports System.Drawing
 Imports DevExpress.Utils.Design
 Imports DevExpress.XtraPrinting
 Imports DevExpress.XtraReports
 Imports DevExpress.XtraReports.UI
-' ...
-#End Region ' #Reference
 
-#Region "#Code"
+' ...
+'#End Region  ' #Reference
+'#Region "#Code"
 Namespace WindowsFormsApplication1
-' The DefaultBindableProperty attribute is intended to make the Position 
-' property bindable when an item is dropped from the Field List.
-<ToolboxItem(True), DefaultBindableProperty("Position"), ToolboxBitmap24Attribute("WindowsFormsApplication1.ProgressBar24x24.png, WindowsFormsApplication11")> _
-Public Class ProgressBar
-    Inherits XRControl
+
+    ' The DefaultBindableProperty attribute is intended to make the Position 
+    ' property bindable when an item is dropped from the Field List.
+    <ToolboxItem(True), DefaultBindableProperty("Position"), ToolboxBitmap24Attribute("WindowsFormsApplication1.ProgressBar24x24.png, WindowsFormsApplication11")>
+    Public Class ProgressBar
+        Inherits XRControl
 
         ' The current position value.
         Private pos As Single = 0
@@ -23,39 +24,37 @@ Public Class ProgressBar
         Private maxVal As Single = 100
 
         Public Sub New()
-            Me.ForeColor = SystemColors.Highlight
+            ForeColor = SystemColors.Highlight
         End Sub
 
         ' Define the MaxValue property.
-        <DefaultValue(100)> _
-        Public Property MaxValue() As Single
+        <DefaultValue(100)>
+        Public Property MaxValue As Single
             Get
-                Return Me.maxVal
+                Return maxVal
             End Get
+
             Set(ByVal value As Single)
-                If value <= 0 Then
-                    Return
-                End If
-                Me.maxVal = value
+                If value <= 0 Then Return
+                maxVal = value
             End Set
         End Property
 
         ' Define the Position property. 
-        <DefaultValue(0), Bindable(True)> _
-        Public Property Position() As Single
+        <DefaultValue(0), Bindable(True)>
+        Public Property Position As Single
             Get
-                Return Me.pos
+                Return pos
             End Get
+
             Set(ByVal value As Single)
-                If value < 0 OrElse value > maxVal Then
-                    Return
-                End If
-                Me.pos = value
+                If value < 0 OrElse value > maxVal Then Return
+                pos = value
             End Set
         End Property
 
         ' Override the XRControl.CreateBrick method.
-        Protected Overrides Function CreateBrick(ByVal childrenBricks() As VisualBrick) As VisualBrick
+        Protected Overrides Function CreateBrick(ByVal childrenBricks As VisualBrick()) As VisualBrick
             ' Use this code to make the progress bar control 
             ' always represented as a Panel brick.
             Return New PanelBrick(Me)
@@ -65,25 +64,19 @@ Public Class ProgressBar
         Protected Overrides Sub PutStateToBrick(ByVal brick As VisualBrick, ByVal ps As PrintingSystemBase)
             ' Call the PutStateToBrick method of the base class.
             MyBase.PutStateToBrick(brick, ps)
-
             ' Get the Panel brick which represents the current progress bar control.
             Dim panel As PanelBrick = CType(brick, PanelBrick)
-
             ' Create a new VisualBrick to be inserted into the panel brick.
-            Dim progressBar As New VisualBrick(Me)
-
+            Dim progressBar As VisualBrick = New VisualBrick(Me)
             ' Hide borders.
             progressBar.Sides = BorderSide.None
-
             ' Set the foreground color to fill the completed area of a progress bar.
             progressBar.BackColor = panel.Style.ForeColor
-
             ' Calculate the rectangle to be filled by the foreground color.
             progressBar.Rect = New RectangleF(0, 0, panel.Rect.Width * (Position / MaxValue), panel.Rect.Height)
-
             ' Add the VisualBrick to the panel.
             panel.Bricks.Add(progressBar)
         End Sub
-End Class
+    End Class
 End Namespace
-#End Region ' #Code
+'#End Region  ' #Code
